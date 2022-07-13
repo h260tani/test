3f965f86171c5a609b7092dfa8708e08735e57f2
h260tani <108774754+h260tani@users.noreply.github.com>
2022-07-13 10:26:24 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <math.h> 
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
+double myRoot(double x)
 {
-  int y;
-  y = 0;
+  double y;
   if( x < 0 ){
     exit(1);
   }
@@ -15,7 +14,7 @@ int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないと�
 
 int main(void)
 {
-  int i;
+  int i = 0;
   double *x = (double *)malloc(sizeof(double) * 100);
 
   while( i <= 100 ){
